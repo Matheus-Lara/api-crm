c029e4e1975104b6a6dfd5172ec1194b440c7708
Matheus-Lara <matheusodelara@gmail.com>
2022-04-16 20:44:59 -0300

    initial commit; Rotas de obtenção de listagem de interações;

---
+++ server.js
@@ -0,0 +1,40 @@
+const MongoClient = require('mongodb').MongoClient;
+MongoClient.connect('mongodb+srv://crm-api:crm-api@cluster0.z9dho.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+.then(client => {
+	const db = client.db('api-crm')
+
+	const express = require('express');
+	const app = express();
+	const cors = require('cors');
+	app.use(cors());
+	app.listen(3000, () => {
+		console.log('listening on 3000');
+	});
+
+	//rotas
+	const baseApiPath = '/api';
+
+	app.get(baseApiPath + '/clientes', (req, res) => {
+		db.collection('clientes').find().toArray()
+		.then(results => {
+			res.send(results);
+		})
+		.catch(error => console.error(error));
+	});
+
+	app.get(baseApiPath + '/interactions/obterInteracoesCliente/:idCliente', (req, res) => {
+		console.log(req.params);
+
+		if (req.params.idCliente) {
+			db.collection('interacoes').find({idCliente: parseInt(req.params.idCliente)}).toArray()
+			.then(results => {
+				res.send(results);
+			})
+			.catch(error => console.error(error));
+		} else {
+			res.json([]);
+		}
+	});
+
+}).catch(console.error)
+
