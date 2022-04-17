1e3962240f1193c9de6bc64322aa2cae89362c8e
flaviajb <flaviajb2@hotmail.com>
2022-04-16 22:11:04 -0300

    Inclusão de rota para atualizar interações;

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
+var ObjectID = require('mongodb').ObjectID;
 MongoClient.connect('mongodb+srv://crm-api:crm-api@cluster0.z9dho.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 .then(client => {
 	const db = client.db('api-crm')
@@ -7,6 +8,7 @@ MongoClient.connect('mongodb+srv://crm-api:crm-api@cluster0.z9dho.mongodb.net/my
 	const app = express();
 	const cors = require('cors');
 	app.use(cors());
+	app.use(express.json());
 	app.listen(3000, () => {
 		console.log('listening on 3000');
 	});
@@ -23,8 +25,6 @@ MongoClient.connect('mongodb+srv://crm-api:crm-api@cluster0.z9dho.mongodb.net/my
 	});
 
 	app.get(baseApiPath + '/interactions/obterInteracoesCliente/:idCliente', (req, res) => {
-		console.log(req.params);
-
 		if (req.params.idCliente) {
 			db.collection('interacoes').find({idCliente: parseInt(req.params.idCliente)}).toArray()
 			.then(results => {
@@ -41,6 +41,21 @@ MongoClient.connect('mongodb+srv://crm-api:crm-api@cluster0.z9dho.mongodb.net/my
 		if (req.params.idInteracao) {
 			db.collection('interacoes').find({idInteracao: parseInt(req.params.idInteracao)})
 			.forEach(doc => res.send(doc));
+		} else {
+			res.json([]);
+		}
+	});
+
+	app.put(baseApiPath + '/interactions/:idInteracao', (req, res) => {
+		const id = req.body._id;
+		delete req.body._id;
+		req.body.idCliente = parseInt(req.body.idCliente);
+		if (req.params.idInteracao) {
+			db.collection('interacoes').updateOne({_id: ObjectID(id)}, {
+				$set: {...req.body}
+			}).then(result => {
+				res.json([]);
+			})
 		} else {
 			res.json([]);
 		}
