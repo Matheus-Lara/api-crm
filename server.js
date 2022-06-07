96469017b8cbfaa404529d048c94becadd868fb5
Matheus-Lara <matheusodelara@gmail.com>
2022-06-07 00:40:55 -0300

    chore: creates docker-structure for database;

--- server.js
+++ server.js
@@ -1,88 +1,26 @@
-const MongoClient = require('mongodb').MongoClient;
-var ObjectID = require('mongodb').ObjectID;
-MongoClient.connect('mongodb+srv://crm-api:crm-api@cluster0.z9dho.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
-.then(client => {
-	const db = client.db('api-crm')
+require('./db/mongo');
 
-	const express = require('express');
-	const app = express();
-	const cors = require('cors');
-	app.use(cors());
-	app.use(express.json());
-	app.listen(3000, () => {
-		console.log('listening on 3000');
-	});
+//imports
+const exp = require('constants');
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const produtoRouter = require('./routes/ProdutoRoute');
 
-	//rotas
-	const baseApiPath = '/api';
+//configurações
+const app = express();
+app.use(morgan('dev'));
+app.use(express.urlencoded({'extended': true}));
+app.use(express.json());
+app.use(cors());
+app.use('/produtos', produtoRouter);
 
-	app.get(baseApiPath + '/clientes', (req, res) => {
-		db.collection('clientes').find().toArray()
-		.then(results => {
-			res.send(results);
-		})
-		.catch(error => console.error(error));
-	});
+//métodos http
+app.get('/', function(req, res) {
+    res.send("Servidor respondeu...");
+});
 
-	app.get(baseApiPath + '/interactions/obterInteracoesCliente/:idCliente', (req, res) => {
-		if (req.params.idCliente) {
-			db.collection('interacoes').find({idCliente: parseInt(req.params.idCliente)}).toArray()
-			.then(results => {
-				res.send(results);
-			})
-			.catch(error => console.error(error));
-		} else {
-			res.json([]);
-		}
-	});
-
-	app.get(baseApiPath + '/interactions/:idInteracao', (req, res) => {
-		if (req.params.idInteracao) {
-			db.collection('interacoes').find({_id: ObjectID(req.params.idInteracao)})
-			.forEach(doc => res.send(doc));
-		} else {
-			res.json([]);
-		}
-	});
-
-	app.put(baseApiPath + '/interactions/:idInteracao', (req, res) => {
-		delete req.body._id;
-		req.body.idCliente = parseInt(req.body.idCliente);
-		if (req.params.idInteracao) {
-			db.collection('interacoes').updateOne({_id: ObjectID(req.params.idInteracao)}, {
-				$set: {...req.body}
-			}).then(result => {
-				res.json([]);
-			});
-		} else {
-			res.json([]);
-		}
-	});
-
-	app.post(baseApiPath + '/interactions', (req, res) => {
-		if (req.body) {
-			delete req.body._id;
-			console.log(req.body);
-			req.body.idCliente = parseInt(req.body.idCliente)
-			db.collection('interacoes').insertOne({
-				...req.body
-			}).then(result => {
-				res.json(result);
-			});
-		} else {
-			res.json([]);
-		}
-	});
-
-	app.delete(baseApiPath + '/interactions/:idInteracao', (req, res) => {
-
-		if (req.params.idInteracao) {
-			db.collection('interacoes').deleteOne({_id: ObjectID(req.params.idInteracao)})
-			.then(() => res.send([]));
-		} else {
-			res.json([]);
-		}
-	});
-
-}).catch(console.error)
-
+//iniciar servidor
+app.listen(3000, function(){
+    console.log("Servidor rodando em localhost:3000");
+});
