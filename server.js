f7bdc8ed69b17bc0475d6ed5f2a02b1b5c634254
Matheus-Lara <matheusodelara@gmail.com>
2022-06-17 20:21:03 -0300

    docs: remove comments;

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 require('./db/mongo');
 
-//imports
 const exp = require('constants');
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const produtoRouter = require('./routes/ProdutoRoute');
 
-//configurações
 const app = express();
 app.use(morgan('dev'));
 app.use(express.urlencoded({'extended': true}));
@@ -15,12 +13,10 @@ app.use(express.json());
 app.use(cors());
 app.use('/produtos', produtoRouter);
 
-//métodos http
 app.get('/', function(req, res) {
     res.send("Servidor respondeu...");
 });
 
-//iniciar servidor
-app.listen(3000, function(){
+app.listen(3000, function() {
     console.log("Servidor rodando em localhost:3000");
 });
